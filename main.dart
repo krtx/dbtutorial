4f2fb97a88f08f349ef4fda97e9f0ab0f6604599
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2019-10-29 23:40:32 +0900

    refactor

--- main.dart
+++ main.dart
@@ -2,45 +2,58 @@ import 'dart:typed_data';
 import 'dart:io';
 import 'dart:convert';
 
-/// 各フィールドのサイズ
-const IdSize = 4;
-const UsernameSize = 32;
-const EmailSize = 255;
-
-/// オフセット
-const IdOffset = 0;
-const UsernameOffset = IdOffset + IdSize;
-const EmailOffset = UsernameOffset + UsernameSize;
-
-/// rowのサイズ
-const RowSize = IdSize + UsernameSize + EmailSize;
-
-/// 1ページのサイズ
-const PageSize = 4096;
-
-/// ページの総数
-const TableMaxPages = 100;
-
-/// 1ページあたりのrowの数
-const RowsPerPage = PageSize / RowSize;
-
-/// 最大row数
-const TableMaxRows = RowsPerPage * TableMaxPages;
-
+/// a row of the table
 class Row {
   int id;
   Uint8List username;
   Uint8List email;
 
-  Row.empty() {}
+  static const IdSize = 4;
+  static const UsernameSize = 32;
+  static const EmailSize = 255;
+  static const Size = IdSize + UsernameSize + EmailSize;
 
   Row(int id, String username, String email) {
-    this.id = id;
-
     var encoder = Utf8Encoder();
-
+    this.id = id;
     this.username = encoder.convert(username);
     this.email = encoder.convert(email);
+  }
+
+  /// deserialize the data from the buffer starting from offset
+  Row.deserialize(Uint8List buffer, int offset) {
+    assert(buffer.length >= offset + Size);
+
+    // id (big endian)
+    this.id = (buffer[offset] << 24) | (buffer[offset + 1] << 16) | (buffer[offset + 2] << 8) | buffer[offset + 3];
+
+    // username
+    this.username = Uint8List.fromList(buffer.skip(offset + IdSize).take(UsernameSize).toList());
+
+    // email
+    this.email = Uint8List.fromList(buffer.skip(offset + IdSize + UsernameSize).take(EmailSize).toList());
+  }
+
+  /// serialize the data into the buffer starting from offset
+  /// the length of serialized list is fixed (RowSize = IdSize + UsernameSize + EmailSize)
+  void serialize(Uint8List buffer, int offset) {
+    assert(buffer.length >= offset + Size);
+
+    // id (big endian)
+    buffer[offset] = (this.id >> 24) & 255;
+    buffer[offset + 1] = (this.id >> 16) & 255;
+    buffer[offset + 2] = (this.id >> 8) & 255;
+    buffer[offset + 3] = this.id & 255;
+
+    // username
+    for (var i = 0; i < this.username.length; i++) {
+      buffer[offset + IdSize + i] = this.username[i];
+    }
+
+    // email
+    for (var i = 0; i < this.email.length; i++) {
+      buffer[offset + IdSize + UsernameSize + i] = this.email[i];
+    }
   }
 
   String toString() {
@@ -49,65 +62,48 @@ class Row {
   }
 }
 
-class ExecuteTableFullException implements Exception {}
-
 class Table {
-  int rows;
+  int numRows;
   List<Uint8List> pages;
 
+  static const PageSize = 4096;
+  static const MaxPages = 100;
+  static const RowsPerPage = PageSize / Row.Size;
+  static const TableMaxRows = RowsPerPage * MaxPages;
+
   Table() {
-    rows = 0;
-    pages = new List(TableMaxPages);
+    numRows = 0;
+    pages = new List(MaxPages);
   }
 
   void add(Row row) {
-    if (this.rows >= TableMaxRows) {
-      throw ExecuteTableFullException();
+    if (this.numRows >= TableMaxRows) {
+      throw 'the table is full';
     }
 
-    int pageIndex = this.rows ~/ RowsPerPage;
+    int pageIndex = this.numRows ~/ RowsPerPage;
     if (this.pages[pageIndex] == null) {
       this.pages[pageIndex] = Uint8List(PageSize);
     }
 
-    int rowOffset = this.rows.remainder(RowsPerPage).toInt();
-    int byteOffset = rowOffset * RowSize;
+    int rowOffset = this.numRows.remainder(RowsPerPage).toInt();
+    int byteOffset = rowOffset * Row.Size;
 
-    // serialize
-    this.pages[pageIndex][byteOffset] = (row.id >> 24) & 255;
-    this.pages[pageIndex][byteOffset + 1] = (row.id >> 16) & 255;
-    this.pages[pageIndex][byteOffset + 2] = (row.id >> 8) & 255;
-    this.pages[pageIndex][byteOffset + 3] = row.id & 255;
-    for (var i = 0; i < row.username.length; i++) {
-      this.pages[pageIndex][i + IdSize + byteOffset] = row.username[i];
-    }
-    for (var i = 0; i < row.email.length; i++) {
-      this.pages[pageIndex][i + IdSize + UsernameSize + byteOffset] = row.email[i];
-    }
+    row.serialize(this.pages[pageIndex], byteOffset);
 
-    this.rows += 1;
+    this.numRows += 1;
   }
 
   List<Row> select() {
     List<Row> rows = [];
 
     // deserialize
-    for (int i = 0; i < this.rows; i++) {
-      Row row = Row.empty();
-
+    for (int i = 0; i < this.numRows; i++) {
       int pageIndex = i ~/ RowsPerPage;
       int rowOffset = i.remainder(RowsPerPage).toInt();
-      int byteOffset = rowOffset * RowSize;
+      int byteOffset = rowOffset * Row.Size;
 
-      row.id = (this.pages[pageIndex][byteOffset] << 24) |
-          (this.pages[pageIndex][byteOffset + 1] << 16) |
-          (this.pages[pageIndex][byteOffset + 2] << 8) |
-          (this.pages[pageIndex][byteOffset + 3]);
-
-      row.username = Uint8List.fromList(this.pages[pageIndex].skip(byteOffset + IdSize).take(UsernameSize).toList());
-      row.email =
-          Uint8List.fromList(this.pages[pageIndex].skip(byteOffset + IdSize + UsernameSize).take(EmailSize).toList());
-
+      Row row = Row.deserialize(this.pages[pageIndex], byteOffset);
       rows.add(row);
     }
 
@@ -117,37 +113,37 @@ class Table {
 
 enum StatementType { insert, select }
 
-class StatementInvalidArgumentsException implements Exception {}
-
-class StatementUnrecognizedException implements Exception {}
-
 class Statement {
+  /// which type the statement is
   StatementType type;
 
+  /// the row to be inserted to table. this variable has a value when the statement is insert
   Row rowToInsert;
 
+  /// parse the user input and prepare for execution
   Statement.prepare(String line) {
-    if (line.startsWith("insert")) {
+    if (line.startsWith('insert')) {
       type = StatementType.insert;
-      var args = line.split(" ");
+      var args = line.split(' ');
 
       if (args.length != 4) {
-        throw StatementInvalidArgumentsException();
+        throw 'insert statement should have 3 arguments';
       }
 
       var id = int.tryParse(args[1]);
       if (id == null) {
-        throw StatementInvalidArgumentsException();
+        throw 'the first argument of insert should be an integer';
       }
 
       rowToInsert = Row(id, args[2], args[3]);
-    } else if (line.startsWith("select")) {
+    } else if (line.startsWith('select')) {
       type = StatementType.select;
     } else {
-      throw StatementUnrecognizedException();
+      throw "unrecognized command: '${line}'";
     }
   }
 
+  /// execute the prepared statement
   execute(Table table) {
     switch (type) {
       case StatementType.insert:
@@ -156,43 +152,48 @@ class Statement {
       case StatementType.select:
         print(table.select());
         break;
-      default:
-        throw Error();
     }
   }
-}
-
-void printPrompt() {
-  stdout.write("db > ");
 }
 
 void main() {
   Table table = Table();
 
+  // repl
   while (true) {
-    printPrompt();
-    String line = stdin.readLineSync();
+    // prompt
+    stdout.write("db > ");
 
+    // read
+    String line = stdin.readLineSync();
     if (line == null) {
       exitCode = 0;
       break;
     }
 
+    // meta commands
     if (line.startsWith(".")) {
       if (line == '.exit') {
         exitCode = 0;
         break;
       } else {
-        stdout.writeln("Unrecognized command: '${line}'.");
+        stdout.writeln("unrecognized command: '${line}'.");
       }
       continue;
     }
 
+    Statement statement;
     try {
-      Statement statement = Statement.prepare(line);
+      statement = Statement.prepare(line);
+    } catch (e) {
+      stderr.writeln("to parse the statement failed: ${e}");
+      continue;
+    }
+
+    try {
       statement.execute(table);
-    } on StatementUnrecognizedException catch (_) {
-      stderr.writeln("Unrecognized command: '${line}'");
+    } catch (e) {
+      stderr.writeln("to execute the statement failed: ${e}");
     }
   }
 }
